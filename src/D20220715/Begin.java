38014e09e5528f50e9a2fbf5392908f15db8fbd5
AntonVotinov <votinovanton@gmail.com>
2022-07-16 09:03:42 +0300

    commit from MSI

---
+++ src/D20220715/Begin.java
@@ -0,0 +1,10 @@
+package D20220715;
+
+public class Begin {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Hello world! Before exit");
+        System.exit(-1);
+        System.out.println("Hello world! After exit");
+    }
+}
--- src/d20220715_Begin.java
+++
@@ -1,5 +0,0 @@
-public class d20220715_Begin {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
