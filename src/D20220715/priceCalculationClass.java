0a96de5805c33f4099cf3755a0a8bb3dad442e5c
AntonVotinov <votinovanton@gmail.com>
2022-07-16 10:51:47 +0300

    commit from MSI

---
+++ src/D20220715/priceCalculationClass.java
@@ -0,0 +1,14 @@
+package D20220715;
+
+public class priceCalculationClass {
+    public static void main(String[] args) {
+        double price = (double) 1/5;
+        int count = 2;
+        System.out.println(priceCalculation(price,count));
+    }
+    public static double priceCalculation(double price, int count) {
+        double resultOfCalculation;
+        resultOfCalculation=price*count;
+        return resultOfCalculation;
+    }
+}
