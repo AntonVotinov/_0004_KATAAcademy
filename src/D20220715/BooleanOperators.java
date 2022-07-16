5bb61eaba3100f49b4021af82f2a0a6ef5ca6f6b
AntonVotinov <votinovanton@gmail.com>
2022-07-16 10:25:56 +0300

    commit from MSI

---
+++ src/D20220715/BooleanOperators.java
@@ -0,0 +1,11 @@
+package D20220715;
+
+public class BooleanOperators {
+    public static void main(String[] args) {
+        boolean a, b, r;
+        a=true;
+        b=false;
+        r = a ^ b;
+        System.out.println(r);
+    }
+}
