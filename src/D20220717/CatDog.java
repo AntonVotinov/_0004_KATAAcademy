835e7e59bf5e2a43cb2e0aef178795ee5afdc216
AntonVotinov <votinovanton@gmail.com>
2022-07-18 09:12:29 +0300

    Commit from Dell

--- src/D20220717/CatDog.java
+++ src/D20220717/CatDog.java
@@ -1,13 +1,13 @@
 package D20220717;
 
 public class CatDog {
-    public static void main(String[] args) {
+    public void main(String[] args) {
         Cat.sayHello();
         Dog.sayHello();
         Dog.catchCat();
     }
     public static class Cat {
-        public static void sayHello() {
+        public void sayHello() {
             System.out.println("Мяу!");
         }
     }
@@ -16,7 +16,7 @@ public class CatDog {
         public void sayHello() {
             System.out.println("Гав!");
         }
-        public static void catchCat(Cat cat) {
+        public void catchCat(Cat cat) {
             System.out.println("Кошка поймана");
             this.sayHello();
             cat.sayHello();
