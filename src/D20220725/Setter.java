c3830a2482c80ada9ef407f490b6bebbdd8d1f84
AntonVotinov <votinovanton@gmail.com>
2022-07-26 08:15:42 +0300

    commit from MSI

---
+++ src/D20220725/Setter.java
@@ -0,0 +1,12 @@
+package D20220725;
+
+public class Setter {
+
+String a = "dd", b = "qwqw";
+
+    public static void main(String[] args) {
+        int a = 1;
+        a++;
+        System.out.println(a);
+    }
+}
