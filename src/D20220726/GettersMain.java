a39857da4f56ccb87169da5e46c69f37bc8f1806
AntonVotinov <votinovanton@gmail.com>
2022-07-26 23:35:04 +0300

    commit from MSI

--- src/D20220726/GettersMain.java
+++ src/D20220726/GettersMain.java
@@ -2,7 +2,7 @@ package D20220726;
 
 public class GettersMain {
     public static void main(String[] args) {
-        Person per = new Person("anton" , 4);
+        Person per = new Person("aaa",3000);
 
         System.out.println(per.getAge() + per.getName());
     }
