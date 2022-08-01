81958d4712bacc0bd88c34abecf309c69d807790
AntonVotinov <votinovanton@gmail.com>
2022-08-01 09:56:07 +0300

    Commit from Dell

---
+++ src/D20220801/Exeptions.java
@@ -0,0 +1,10 @@
+package D20220801;
+
+public class Exeptions {
+    public static void main(String[] args) {
+        Object b = "1.222";
+        Integer a = (Integer) b;
+        System.out.println(a);
+    }
+}
+
